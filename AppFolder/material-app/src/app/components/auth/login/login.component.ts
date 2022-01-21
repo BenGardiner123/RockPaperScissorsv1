398dbcb6ff0af5a6d8e52ba8c600a5bd9c9c5645
BenGardiner123 <61527372+BenGardiner123@users.noreply.github.com>
2022-01-21 17:05:18 +1100

    adding login component - using reactive forms

--- AppFolder/material-app/src/app/components/auth/login/login.component.ts
+++ AppFolder/material-app/src/app/components/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup
 
-  ngOnInit(): void {
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
+  
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    })
+
   }
 
+
+  onLoginSubmit() {
+    console.log(this.loginForm);
+  }
 }
